12c4552eb3d089d2372fbd8b8afc1cee8ae87a19
slurpeecup <zaidwyasin@gmail.com>
2022-09-03 03:29:27 -0700

    468

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+// COMPE271 HW 1 Zaid Yasin.cpp
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+#include "game.h"
+
+int main()
+{
+    int game_table[25][25];
+
+
+board_init(**game_table);
+return 0;
+};
+
